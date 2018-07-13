eb794fc00e849c3ffa8e8b0ff29a2777fa8ce601
Jamsterman25 <33459850+Jamsterman25@users.noreply.github.com>
2018-07-13 10:01:24 -0500

    Implements dual mode UART (USB or GPIO)

--- MSP430Code/BluetoothHeadphoneSplitter.c
+++ MSP430Code/BluetoothHeadphoneSplitter.c
@@ -43,107 +43,294 @@
  *
  * --/COPYRIGHT--*/
 //******************************************************************************
-//   MSP430FR5x9x Demo - eUSCI_B1, SPI 3-Wire Master Incremented Data
+//  MSP430FR5x9x Demo - eUSCI_A3 UART echo at 9600 baud using BRCLK = 8MHz
 //
-//   Description: SPI master talks to SPI slave using 3-wire mode. Incrementing
-//   data is sent by the master starting at 0x01. Received data is expected to
-//   be same as the previous transmission TXData = RXData-1.
-//   USCI RX ISR is used to handle communication with the CPU, normally in LPM0.
-//   ACLK = 32.768kHz, MCLK = SMCLK = DCO ~1MHz.  BRCLK = ACLK/2
+//  Description: This demo echoes back characters received via a PC serial port.
+//  SMCLK/ DCO is used as a clock source and the device is put in LPM3
+//  The auto-clock enable feature is used by the eUSCI and SMCLK is turned off
+//  when the UART is idle and turned on when a receive edge is detected.
+//  Note that level shifter hardware is needed to shift between RS232 and MSP
+//  voltage levels.
 //
+//  The example code shows proper initialization of registers
+//  and interrupts to receive and transmit data.
+//  To test code in LPM3, disconnect the debugger.
 //
-//                   MSP430FR5994
-//                 -----------------
-//            /|\ |              XIN|-
-//             |  |                 |  32KHz Crystal
-//             ---|RST          XOUT|-
-//                |                 |
-//                |             P5.0|-> Data Out (UCB1SIMO)
-//                |                 |
-//                |             P5.1|<- Data In (UCB1SOMI)
-//                |                 |
-//                |             P5.2|-> Serial Clock Out (UCB1CLK)
+//  ACLK = VLO, MCLK =  DCO = SMCLK = 8MHz
+//
+//                MSP430FR5994
+//             -----------------
+//       RST -|     P6.0/UCA3TXD|----> PC (echo)
+//            |                 |
+//            |                 |
+//            |     P6.1/UCA3RXD|<---- PC
+//            |                 |
 //
 //   William Goh
 //   Texas Instruments Inc.
 //   October 2015
 //   Built with IAR Embedded Workbench V6.30 & Code Composer Studio V6.1
 //******************************************************************************
+/* Adaptation for Texas Tech University ECE Project Lab 4
+ *
+ * Tony Kahumbu & Jeremiah McCutcheon Copyright (c) 2018
+ *
+ *  This project utilizes UART communication to control 4 Bluetooth modules
+ * each module can connect to a device and should be completly configured using
+ * this code.
+ *
+ *  NOTE: If switching to USB-UART (setup function included) you must adjust the interrupt vector from A3 to A0
+ *
+ *
+ */
 #include <msp430.h>
+voic CLOCK_SETUP();
+void USB_UART_SETUP();
+void GPIO_UART_SETUP();
 
-volatile unsigned char RXData = 0;
-volatile unsigned char TXData;
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;               // Stop Watchdog
 
-    // Configure GPIO
-    P5SEL1 &= ~(BIT0 | BIT1 | BIT2);        // USCI_B1 SCLK, MOSI, and MISO pin
-    P5SEL0 |= (BIT0 | BIT1 | BIT2);
-    PJSEL0 |= BIT4 | BIT5;                  // For XT1
+
+    */
+    //GPIO-UART
+
 
     // Disable the GPIO power-on default high-impedance mode to activate
     // previously configured port settings
     PM5CTL0 &= ~LOCKLPM5;
 
-    // XT1 Setup
-    CSCTL0_H = CSKEY_H;                     // Unlock CS registers
-    CSCTL1 = DCOFSEL_0;                     // Set DCO to 1MHz
-    CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK;
-    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // set all dividers
-    CSCTL4 &= ~LFXTOFF;
-    do
-    {
-        CSCTL5 &= ~LFXTOFFG;                // Clear XT1 fault flag
-        SFRIFG1 &= ~OFIFG;
-    } while (SFRIFG1 & OFIFG);              // Test oscillator fault flag
-    CSCTL0_H = 0;                           // Lock CS registers
-
-    // Configure USCI_B1 for SPI operation
-    UCB1CTLW0 = UCSWRST;                    // **Put state machine in reset**
-    UCB1CTLW0 |= UCMST | UCSYNC | UCCKPL | UCMSB; // 3-pin, 8-bit SPI master
-                                            // Clock polarity high, MSB
-    UCB1CTLW0 |= UCSSEL__ACLK;              // ACLK
-    UCB1BRW = 0x02;                         // /2
-    //UCB1MCTLW = 0;                          // No modulation
-    UCB1CTLW0 &= ~UCSWRST;                  // **Initialize USCI state machine**
-    UCB1IE |= UCRXIE;                       // Enable USCI_B1 RX interrupt
-    TXData = 0x1;                           // Holds TX data
-
-    while(1)
-    {
-        UCB1IE |= UCTXIE;
-        __bis_SR_register(LPM0_bits | GIE); // CPU off, enable interrupts
-        __delay_cycles(2000);               // Delay before next transmission
 
 
-        TXData++;                           // Increment transmit data
-    }
+    /* USB-UART
+
+    */
+    // GPIO-UART
+
+
+
+    __bis_SR_register(LPM3_bits | GIE);     // Enter LPM3, interrupts enabled
+    __no_operation();                       // For debugger
 }
 
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=EUSCI_B1_VECTOR
-__interrupt void USCI_B1_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(EUSCI_B1_VECTOR))) USCI_B1_ISR (void)
-#else
-#error Compiler not supported!
-#endif
+#pragma vector=EUSCI_A0_VECTOR
+__interrupt void USCI_A3_ISR(void)
+
 {
-    switch(__even_in_range(UCB1IV, USCI_SPI_UCTXIFG))
+    switch(__even_in_range(UCA0IV, USCI_UART_UCTXCPTIFG))
     {
         case USCI_NONE: break;
-        case USCI_SPI_UCRXIFG:
-            RXData = UCB1RXBUF;
-            UCB1IFG &= ~UCRXIFG;
-            __bic_SR_register_on_exit(LPM0_bits); // Wake up to setup next TX
+        case USCI_UART_UCRXIFG:
+            while(!(UCA0IFG&UCTXIFG));
+            UCA0TXBUF = UCA0RXBUF;
+            __no_operation();
             break;
-        case USCI_SPI_UCTXIFG:
-            UCB1TXBUF = TXData;                   // Transmit characters
-            UCB1IE &= ~UCTXIE;
-            break;
+        case USCI_UART_UCTXIFG: break;
+        case USCI_UART_UCSTTIFG: break;
+        case USCI_UART_UCTXCPTIFG: break;
         default: break;
     }
 }
 
+void CLOCK_SETUP(){
+    // Startup clock system with max DCO setting ~8MHz
+    CSCTL0_H = CSKEY_H;                     // Unlock CS registers
+    CSCTL1 = DCOFSEL_3 | DCORSEL;           // Set DCO to 8MHz
+    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;   // Set all dividers
+    CSCTL0_H = 0;                           // Lock CS registers
+}
+
+void USB_UART_SETUP(){
+    // Configure GPIO
+    P2SEL1 |= (BIT0 | BIT1); // USCI_A0 UART operation
+    P2SEL0 &= ~(BIT0 | BIT1);
+
+    // Configure USCI_A0 for UART mode
+    UCA0CTLW0 = UCSWRST;                    // Put eUSCI in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK;             // CLK = SMCLK
+    // Baud Rate calculation
+    // 8000000/(16*9600) = 52.083
+    // Fractional portion = 0.083
+    // User's Guide Table 21-4: UCBRSx = 0x04
+    // UCBRFx = int ( (52.083-52)*16) = 1
+    UCA0BRW = 52;                           // 8000000/16/9600
+    UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
+    UCA0CTLW0 &= ~UCSWRST;                  // Initialize eUSCI
+    UCA0IE |= UCRXIE;                       // Enable USCI_A0 RX interrupt
+}
+
+void GPIO_UART_SETUP(){
+    // Configure GPIO
+    P6SEL1 &= ~(BIT0 | BIT1);
+    P6SEL0 |= (BIT0 | BIT1);                // USCI_A3 UART operation
+
+    // Configure USCI_A0 for UART mode
+    UCA3CTLW0 = UCSWRST;                    // Put eUSCI in reset
+    UCA3CTLW0 |= UCSSEL__SMCLK;             // CLK = SMCLK
+    // Baud Rate calculation
+    // 8000000/(16*9600) = 52.083
+    // Fractional portion = 0.083
+    // User's Guide Table 21-4: UCBRSx = 0x04
+    // UCBRFx = int ( (52.083-52)*16) = 1
+    UCA3BRW = 52;                           // 8000000/16/9600
+    UCA3MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
+    UCA3CTLW0 &= ~UCSWRST;                  // Initialize eUSCI
+    UCA3IE |= UCRXIE;                       // Enable USCI_A0 RX interrupt
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  ECE 3362 project 6 UART Communication. This project will recieve an interrupt
+    from a keyboard input and return the pressed key to the console.
+   Target: TI LaunchPad development board with MSP430G2553 device with the
+    a speaker with DC blocking capacitor installed at P2.3 with common side
+    of speaker grounded
+       Date:           May 6, 2017
+       Last Revision:  1.0
+       Written by:     Jeremiah McCutcheon
+       Adapted from:   Dr. Michael Helm, Simple UART example.
+       & Troy Davis: UART Interrupt Example.
+       Assembler/IDE:  IAR Embedded Workbench 6.5
+        HW I/O assignments:
+        P1.0    LED1    (Active HIGH)RED
+        P1.1    not used   UART RX serial data (input to MSP430)
+        P1.2    not used   UART TX serial data (output from MSP430)
+        P1.3    not used PushButton (Active LOW) (internal Pullup Enabled)
+        P1.4    not used
+        P1.5    not used
+        P1.6    not used LED2    (Active HIGH)GREEN
+        P1.7    not used
+
+        P2.0    not used
+        P2.1    not used
+        P2.2    not used
+        P2.3    not used
+        P2.4    not used
+        P2.5    not used
+        P2.6    not used
+        P2.7    not used
+*/
+/*
+// Device Library
+#include <msp430.h>
+
+// Program Constants
+
+// UART
+#define UART_RX 0x02    // P1.1
+#define UART_TX 0x04    // P1.2
+#define UART_BR 109     // UART Baud Rate = 9600 baud at 1 MHz
+
+// Function prototypes
+void uartSetup();
+void timerSetup();
+void Send(char * tx_data);
+void delay(unsigned int sec);
+
+// Variables
+unsigned char rxArray[256];
+unsigned int rxIndex = 0;
+static char data;
+char* temp = " ";
+
+void main(void) {
+     WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer
+
+    uartSetup();
+
+    rxIndex = 0;
+
+    P2DIR |= 0x03u;
+
+    _BIS_SR(GIE);
+
+    P2OUT = 0x00; // Set to ch0
+
+    while(1){
+        Send("get name\r\n");
+        delay(2);
+        //delay(2);
+    }
+
+}
+
+
+void uartSetup()
+{
+        DCOCTL=0;
+        BCSCTL1 = CALBC1_1MHZ; // Set DCO
+        DCOCTL = CALDCO_1MHZ;
+    // Set up UART
+        UCA0CTL1 |= UCSWRST;            // Disable UART
+        P1SEL |= UART_TX + UART_RX;     // Set port pins to UART mode
+        P1SEL2 |= UART_TX + UART_RX;    // Set port pins to UART mode
+        P1DIR |= UART_TX;               // Set TX pin as output
+        UCA0CTL1 |= UCSSEL_2;           // Set UART to use SMCLK (1 MHz)
+        UCA0BR0 = 109;                  // Set baud rate: 9600
+        UCA0BR1 = 0;
+        UCA0MCTL = UCBRS_0;             // UART Modulation Control setup
+        UCA0CTL1 &= ~UCSWRST;           // Enable UART
+        IE2 |= UCA0RXIE;                // Enable USCI_A0 RX interrupt
+}
+
+void delay(unsigned int sec){
+    while(sec>0){
+        __delay_cycles(1000000);
+        sec--;
+    }
+}
+
+void Send(char * tx_data) // Send Out TX
+{
+    unsigned int i=0;
+
+    while(tx_data[i]) // Increment
+    {
+        while ((UCA0STAT & UCBUSY)); // Wait
+        UCA0TXBUF = tx_data[i]; // Send out element
+        i++;
+    }
+}
+
+#pragma vector=USCIAB0RX_VECTOR  // Receive UART interrupt
+__interrupt void USCI0RX_ISR(void)
+{
+    data = UCA0RXBUF;
+
+    while ((UCA0STAT & UCBUSY)); // Wait
+    rxArray[rxIndex] = data; // Copy element to array
+    rxIndex++;
+
+}
+*/
