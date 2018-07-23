9d474516fb187e5fca11f34fc3561215f861625d
Jamsterman25 <33459850+Jamsterman25@users.noreply.github.com>
2018-07-23 18:59:29 -0500

    Working with LCD screen

--- MSP430Code/BluetoothHeadphoneSplitter.c
+++ MSP430Code/BluetoothHeadphoneSplitter.c
@@ -71,6 +71,59 @@
 //   October 2015
 //   Built with IAR Embedded Workbench V6.30 & Code Composer Studio V6.1
 //******************************************************************************
+
+
+// ------------------------------------------------------------------------------------
+// SECONDARY ADAPTED CODE:
+// Author: Troy Davis
+// Company: Texas Tech University
+// Department: Pulsed Power and Power Electronics Lab (P3E)
+// Status: Incomplete
+//
+// Date: 6/14/2017
+// Assembler: Code Composer Studio 7.0.0
+// Target Board: TI MSP430G2553 LaunchPad
+//
+// Flash Used: 2Kb
+// RAM Used: .1Kb
+// I/O Pins Used: 14
+//
+// Description: Show different things depending on the button pressed, etc, etc
+//      You have to figure out which pin is which on the number pad.
+//      You can test this with 3.3V and a multimeter. Just hook up 3.3 to a pin
+//      and test each other pins for 3.3 when a button is pressed.
+//
+// I/O Assignments: Port 1 will mostly be used by the 16x2 LCD screen and some extras.
+//
+//      16x2 Screen + Extra:
+//          P3.0: (output) LCD D4
+//          P3.1: (output) LCD D5
+//          P3.2: (output) LCD D6
+//          P3.3: (output) LCD D7
+//          P3.4: (output) LCD E
+//          P3.5: (output) LCD RS
+//          P3.6: (TBD)
+//          P3.7: (TBD)
+//
+
+//
+//      Extra Connections:
+//          LCD VDD - 5V (TP1 on MSP430 LaunchPad)
+//          LCD VSS - GND
+//          LCD RW  - GND
+//          LCD V0  - 3V through pot (adjusts contrast)
+//          LCD K   - GND
+//          LCD A   - 5V through pot (adjusts brightness)
+//
+//      Needed Connections:
+//          Thermistor?   // external temp
+//          PWM out?
+//          Voltage?
+//
+// Comments: In order to do in-circuit programming of the MSP if the chip is on a breadboard,
+//           one must follow these schematics: http://www.ti.com/lit/ug/slau318g/slau318g.pdf
+//
+// -------------------------------------------------------------------------------------
 /* Adaptation for Texas Tech University ECE Project Lab 4
  *
  * Tony Kahumbu & Jeremiah McCutcheon Copyright (c) 2018
@@ -84,13 +137,45 @@
  *
  */
 #include <msp430.h>
+
+#define lcd_port        P3OUT
+#define lcd_port_dir    P3DIR
+
+#define LCD_EN      BIT4
+#define LCD_RS      BIT5
+#define top         0x80 // top line of LCD starts here and goes to 8F
+#define bottom      0xC0 // bottom line of LCD starts here and goes to CF
+
+#define a           0
+#define b           1
+#define c           2
+#define d           3
+
+struct BTDevice{
+    char MAC[10];
+    int RSSI;
+    char * NAME;
+};
+
 void CLOCK_SETUP();
 void USB_UART_SETUP();
 void GPIO_UART_SETUP();
 void Send(char * input);
 void delay(int sec);
+void Inquiry();
 
-unsigned char RXED[256]; // Received data
+void lcd_reset();
+void lcd_pos(char pos);
+void lcd_setup();
+void lcd_data(unsigned char dat);
+void lcd_display_top(char *line);
+void lcd_display_bottom(char * line);
+void lcd_clear_top(char * line);
+void lcd_clear_bottom(char * line);
+
+struct BTDevice BTVector[10];
+
+unsigned char RXED[1024]; // Received data
 unsigned int RXI = 0;    // Received data increment
 
 int main(void)
@@ -106,9 +191,15 @@ int main(void)
 
     __bis_SR_register(GIE);     // Enter LPM3, interrupts enabled
 
+    lcd_setup();
+
     while(1){
-        Send("INQUIRY 1 \r");
-        delay(4);
+        lcd_display_top("Select Device:");
+        lcd_display_bottom("Jamoji JK");
+        delay(3);
+        lcd_display_top("Select Device:");
+        lcd_display_bottom("BEARDEDBLUE CLASSIC");
+        delay(1);
     }
 
 }
@@ -122,6 +213,8 @@ __interrupt void USCI_A3_ISR(void)
         case USCI_NONE: break;
         case USCI_UART_UCRXIFG:
             RXED[RXI] = UCA3RXBUF;
+            if(RXI > 1023)
+                RXI = 0;
             RXI++;
             break;
         case USCI_UART_UCTXIFG: break;
@@ -131,9 +224,19 @@ __interrupt void USCI_A3_ISR(void)
     }
 }
 
+void Inquiry(){
+    RXI = 0;
+    Send("Inquiry 5\r");
+    delay(6);
+    unsigned int i = 0;
+    for(i=0; i=RXI; i++){
+
+    }
+}
+
 void Send(char * input){
     int i=0;
-    while (input){
+    while (input[i]){
         while(!(UCA3IFG&UCTXIFG)); // Wait
         UCA3TXBUF = input[i];     // Send
         i++;
@@ -188,35 +291,93 @@ void GPIO_UART_SETUP(){
 }
 
 void delay(int sec){
-    while (sec<0){
-        __delay_cycles(1000000); // Delay 1s
+    while (sec>0){
+        __delay_cycles(8000000); // Delay 1s
         sec--;
     }
 }
 
+void lcd_reset()
+{
+    lcd_port_dir = 0xff;
+    lcd_port = 0xff;
+    __delay_cycles(160000);
+    lcd_port = 0x03+LCD_EN;
+    lcd_port = 0x03;
+    __delay_cycles(80000);
+    lcd_port = 0x03+LCD_EN;
+    lcd_port = 0x03;
+    __delay_cycles(8000);
+    lcd_port = 0x03+LCD_EN;
+    lcd_port = 0x03;
+    __delay_cycles(8000);
+    lcd_port = 0x02+LCD_EN;
+    lcd_port = 0x02;
+    __delay_cycles(8000);
+}
+
+void lcd_pos (char pos)
+{
+    lcd_port = ((pos >> 4) & 0x0F)|LCD_EN;
+    lcd_port = ((pos >> 4) & 0x0F);
+
+    lcd_port = (pos & 0x0F)|LCD_EN;
+    lcd_port = (pos & 0x0F);
+
+    __delay_cycles(32000);
+}
+
+void lcd_setup()
+{
+    lcd_reset();         // Call LCD reset
+    lcd_pos(0x28);       // 4-bit mode - 2 line - 5x7 font.
+    lcd_pos(0x0C);       // Display no cursor - no blink.
+    lcd_pos(0x06);       // Automatic Increment - No Display shift.
+    lcd_pos(0x80);       // Address DDRAM with 0 offset 80h.
+    lcd_pos(0x01);       // Clear screen
+}
 
 
+void lcd_data (unsigned char dat)
+{
+    lcd_port = (((dat >> 4) & 0x0F)|LCD_EN|LCD_RS);
+    lcd_port = (((dat >> 4) & 0x0F)|LCD_RS);
 
+    lcd_port = ((dat & 0x0F)|LCD_EN|LCD_RS);
+    lcd_port = ((dat & 0x0F)|LCD_RS);
 
+    __delay_cycles(3200);
+}
 
+void lcd_display_top(char * line)
+{
+    lcd_pos(top);
+    while (*line)
+        lcd_data(*line++);
+}
 
+void lcd_clear_top(char * line)
+{
+    lcd_clear_top("                ");
+    lcd_pos(top);
+    while (*line)
+        lcd_data(*line++);
+}
 
+void lcd_display_bottom(char * line)
+{
+    lcd_clear_bottom("                ");
+    lcd_pos(bottom);
+    while (*line)
+        lcd_data(*line++);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void lcd_clear_bottom(char * line)
+{
+    lcd_pos(bottom);
+    while (*line)
+        lcd_data(*line++);
+}
 
 
 
